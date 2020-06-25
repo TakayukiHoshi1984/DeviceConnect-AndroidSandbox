4777d81a145af426e9e2628b192dd27c3ebb724c
TakayukiHoshi1984 <hoshi@gclue.jp>
2020-06-25 18:59:10 +0900

    IPアドレスを調べる処理を追加してみた

--- emulator-install.sh
+++ emulator-install.sh
@@ -2,6 +2,8 @@
 
 echo "Hellow World!"
 
+/Users/runner/android-sdk/platform-tools/adb shell ifconfig eth0
+
 /Users/runner/android-sdk/platform-tools/adb install -r ./dConnectManager/dConnectManager/dconnect-manager-app/build/outputs/apk/debug/dconnect-manager-app-debug.apk
 
 /Users/runner/android-sdk/platform-tools/adb install -r ./dConnectDevicePlugin/dConnectDeviceTest/app/build/outputs/apk/debug/app-debug.apk
