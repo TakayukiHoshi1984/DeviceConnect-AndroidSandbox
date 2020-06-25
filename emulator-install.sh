e4a7bc0976bcd544d2cb0b34187e32ac70f0dd37
TakayukiHoshi1984 <hoshi@gclue.jp>
2020-06-25 18:19:18 +0900

    curlの

--- emulator-install.sh
+++ emulator-install.sh
@@ -7,10 +7,17 @@ echo "Hellow World!"
 /Users/runner/android-sdk/platform-tools/adb install -r ./dConnectDevicePlugin/dConnectDeviceTest/app/build/outputs/apk/debug/app-debug.apk
 
 
-# /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
-# /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/.DConnectLaunchActivity -d gotapi://start/server
-pwd
+/Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/.DConnectLaunchActivity -d gotapi://start/server
+/Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
+
+curl  -X GET \
+      -H 'Origin: localhost' \
+      http://localhost:4035/gotapi/availability
+      
+curl  -X GET \
+      -H 'Origin: localhost' \
+      http://localhost:4035/gotapi/serviceDiscovery
 cd dConnectManager/dConnectManager
 chmod +x ./gradlew
 ./gradlew dconnect-server-nano-httpd:connectedAndroidTest
-# ./gradlew dconnect-manager-app:connectedAndroidTest --stacktrace
+./gradlew dconnect-manager-app:connectedAndroidTest --stacktrace
