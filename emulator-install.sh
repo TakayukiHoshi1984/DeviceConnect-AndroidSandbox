6163b4109ac28f61fe0602570660763a55ee09f9
TakayukiHoshi1984 <hoshi@gclue.jp>
2020-06-25 14:00:30 +0900

    apkのインストール処理を追加。

--- emulator-install.sh
+++ emulator-install.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 echo "Hellow World!"
+
+/Users/runner/android-sdk/platform-tools/adb install -r ./dConnectManager/dConnectManager/dconnect-manager-app/build/outputs/apk/debug/dconnect-manager-app-debug.apk
+
+/Users/runner/android-sdk/platform-tools/adb install -r ./dConnectDevicePlugin/dConnectDeviceTest/app/build/outputs/apk/debug/app-debug.apk
+
+chmod +x /dConnectManager/dConnectManager/gradlew
+./dConnectManager/dConnectManager/gradlew connectedCheck --stacktrace
