a2fc09fdac1a869e0a6c3b5d270b981d1c0e7828
TakayukiHoshi1984 <hoshi@gclue.jp>
2020-06-25 18:13:08 +0900

    Managerのテストのコメントアウト

--- emulator-install.sh
+++ emulator-install.sh
@@ -7,11 +7,10 @@ echo "Hellow World!"
 /Users/runner/android-sdk/platform-tools/adb install -r ./dConnectDevicePlugin/dConnectDeviceTest/app/build/outputs/apk/debug/app-debug.apk
 
 
-/Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
-
-/Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/.DConnectLaunchActivity -d gotapi://start/server
+# /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
+# /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/.DConnectLaunchActivity -d gotapi://start/server
 pwd
 cd dConnectManager/dConnectManager
 chmod +x ./gradlew
 ./gradlew dconnect-server-nano-httpd:connectedAndroidTest
-./gradlew dconnect-manager-app:connectedAndroidTest --stacktrace
+# ./gradlew dconnect-manager-app:connectedAndroidTest --stacktrace
