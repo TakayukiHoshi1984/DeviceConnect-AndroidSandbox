9105028df543db5b3b226ee8be6b7f6f31398668
TakayukiHoshi1984 <hoshi@gclue.jp>
2020-06-26 12:44:41 +0900

    コマンドの順番修正

--- emulator-install.sh
+++ emulator-install.sh
@@ -8,18 +8,19 @@ echo "Hellow World!"
 
 /Users/runner/android-sdk/platform-tools/adb install -r ./dConnectDevicePlugin/dConnectDeviceTest/app/build/outputs/apk/debug/app-debug.apk
 
-# /Users/runner/android-sdk/platform-tools/adb shell | sudo ifconfig
+/Users/runner/android-sdk/platform-tools/adb shell ; sudo ifconfig ; exit
+
+/Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
 
 /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/.DConnectLaunchActivity -d gotapi://start/server
-/Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
 
 
 curl  -X GET \
-      -H 'Origin: localhost' \
+      -H 'Origin: 10.79.2.176' \
       http://10.79.2.176:4035/gotapi/availability
       
 curl  -X GET \
-      -H 'Origin: localhost' \
+      -H 'Origin: 10.79.2.176' \
       http://10.79.2.176:4035/gotapi/serviceDiscovery
 cd dConnectManager/dConnectManager
 chmod +x ./gradlew
