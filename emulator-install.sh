cb5f4a9f153c02bfff18f14bc2c942b8ec05eb1f
TakayukiHoshi1984 <hoshi@gclue.jp>
2020-06-26 15:22:18 +0900

    ホスト名を修正

--- emulator-install.sh
+++ emulator-install.sh
@@ -2,25 +2,25 @@
 
 echo "Hellow World!"
 
-# /Users/runner/android-sdk/platform-tools/adb install -r ./dConnectManager/dConnectManager/dconnect-manager-app/build/outputs/apk/debug/dconnect-manager-app-debug.apk
+/Users/runner/android-sdk/platform-tools/adb install -r ./dConnectManager/dConnectManager/dconnect-manager-app/build/outputs/apk/debug/dconnect-manager-app-debug.apk
 
-# /Users/runner/android-sdk/platform-tools/adb install -r ./dConnectDevicePlugin/dConnectDeviceTest/app/build/outputs/apk/debug/app-debug.apk
+/Users/runner/android-sdk/platform-tools/adb install -r ./dConnectDevicePlugin/dConnectDeviceTest/app/build/outputs/apk/debug/app-debug.apk
 
-/Users/runner/android-sdk/platform-tools/adb shell | sudo ifconfig | logout
+# /Users/runner/android-sdk/platform-tools/adb shell | sudo ifconfig | logout
 
-# /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
+/Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
 
-# /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/.DConnectLaunchActivity -d gotapi://start/server
+/Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/.DConnectLaunchActivity -d gotapi://start/server
 
 
-# curl  -X GET \
-#       -H 'Origin: localhost' \
-#       http://127.0.0.1:4035/gotapi/availability
+curl  -X GET \
+      -H 'Origin: localhost' \
+      http://github.com:4035/gotapi/availability
       
-# curl  -X GET \
-#       -H 'Origin: 10.79.2.176' \
-#       http://10.79.2.176:4035/gotapi/serviceDiscovery
-# cd dConnectManager/dConnectManager
-# chmod +x ./gradlew
-# ./gradlew dconnect-server-nano-httpd:connectedAndroidTest
-# ./gradlew dconnect-manager-app:connectedAndroidTest --stacktrace
+curl  -X GET \
+      -H 'Origin: 10.79.2.176' \
+      http://github.com:4035/gotapi/serviceDiscovery
+cd dConnectManager/dConnectManager
+chmod +x ./gradlew
+./gradlew dconnect-server-nano-httpd:connectedAndroidTest
+./gradlew dconnect-manager-app:connectedAndroidTest --stacktrace
