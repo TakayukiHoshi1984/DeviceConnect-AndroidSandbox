0d50dcb9c74419bc769d2b7b7383533340c0e158
TakayukiHoshi1984 <hoshi@gclue.jp>
2020-06-29 10:18:33 +0900

    adb shellの処理を追加

--- emulator-install.sh
+++ emulator-install.sh
@@ -6,7 +6,7 @@ echo "Hellow World!"
 
 /Users/runner/android-sdk/platform-tools/adb install -r ./dConnectDevicePlugin/dConnectDeviceTest/app/build/outputs/apk/debug/app-debug.apk
 
-# /Users/runner/android-sdk/platform-tools/adb shell | sudo ifconfig
+ /Users/runner/android-sdk/platform-tools/adb shell | sudo ipconfig getifaddr en0
 # logout
 
 # /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
@@ -15,14 +15,13 @@ echo "Hellow World!"
 
 # /Users/runner/android-sdk/platform-tools/adb forward tcp:4035 tcp:4035
 
-brew update
 
 # curl  -X GET \
 #       -H 'Origin: `ipconfig getifaddr en0`' \
 #       http://`ipconfig getifaddr en0`:4035/gotapi/availability
-curl  -X GET \
-      -H 'Origin: localhost' \
-      http://localhost:4035/gotapi/availability
+# curl  -X GET \
+#       -H 'Origin: localhost' \
+#       http://localhost:4035/gotapi/availability
       
 # curl  -X GET \
 #       -H 'Origin: 10.79.2.176' \
