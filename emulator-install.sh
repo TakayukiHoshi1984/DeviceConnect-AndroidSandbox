61bb95d72d6681cf40add1842796ecd6ebf2becd
TakayukiHoshi1984 <hoshi@gclue.jp>
2020-06-29 13:46:47 +0900

    auth tokenの追加

--- emulator-install.sh
+++ emulator-install.sh
@@ -15,7 +15,10 @@ echo "Hellow World!"
 
 # /Users/runner/android-sdk/platform-tools/adb forward tcp:4035 tcp:4035
 
-telnet `ipconfig getifaddr en0` 5554 ; redir add tcp:4035:4035 ; list
+cat /Users/runner/.emulator_console_auth_token
+
+
+# telnet localhost 5554 ; redir add tcp:4035:4035 ; list
 
 
 # curl  -X GET \
