ccab15352c5d5205623b13cfd639ce7c0fd1587a
TakayukiHoshi1984 <hoshi@gclue.jp>
2020-06-29 10:42:20 +0900

    telnetの追加

--- emulator-install.sh
+++ emulator-install.sh
@@ -6,7 +6,7 @@ echo "Hellow World!"
 
 /Users/runner/android-sdk/platform-tools/adb install -r ./dConnectDevicePlugin/dConnectDeviceTest/app/build/outputs/apk/debug/app-debug.apk
 
- /Users/runner/android-sdk/platform-tools/adb shell ; ipconfig getifaddr en0 ; exit
+#/Users/runner/android-sdk/platform-tools/adb shell ; ipconfig getifaddr en0 ; exit
 # logout
 
 # /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
@@ -15,13 +15,17 @@ echo "Hellow World!"
 
 # /Users/runner/android-sdk/platform-tools/adb forward tcp:4035 tcp:4035
 
+telnet localhost 5554
+redir add tcp:4035:4035
+list
+
 
 # curl  -X GET \
 #       -H 'Origin: `ipconfig getifaddr en0`' \
 #       http://`ipconfig getifaddr en0`:4035/gotapi/availability
-# curl  -X GET \
-#       -H 'Origin: localhost' \
-#       http://localhost:4035/gotapi/availability
+curl  -X GET \
+      -H 'Origin: localhost' \
+      http://localhost:4035/gotapi/availability
       
 # curl  -X GET \
 #       -H 'Origin: 10.79.2.176' \
