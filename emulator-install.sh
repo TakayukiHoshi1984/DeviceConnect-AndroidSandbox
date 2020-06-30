fd8cdb88dd5e4848a31bd4ae5da5ef9a5bc0b7e5
Takayuki Hoshi <hoshi@gclue.jp>
2020-06-30 17:07:00 +0900

    emulator-install.shの更新。

--- emulator-install.sh
+++ emulator-install.sh
@@ -6,35 +6,29 @@ echo "Hellow World!"
 
 /Users/runner/android-sdk/platform-tools/adb install -r ./dConnectDevicePlugin/dConnectDeviceTest/app/build/outputs/apk/debug/app-debug.apk
 
-#/Users/runner/android-sdk/platform-tools/adb shell ; ipconfig getifaddr en0 ; exit
-# logout
+# /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/.DConnectLaunchActivity -d gotapi://start/server
 
-# /Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/org.deviceconnect.android.manager.setting.ServiceListActivity
 
-/Users/runner/android-sdk/platform-tools/adb shell am start -n org.deviceconnect.android.manager/.DConnectLaunchActivity -d gotapi://start/server
-
-# /Users/runner/android-sdk/platform-tools/adb forward tcp:4035 tcp:4035
-
-AUTH=`cat /Users/runner/.emulator_console_auth_token`
-
-echo $AUTH 
-
-(sleep 1 ; echo auth $AUTH ; sleep 1 ; echo redir add tcp:4035:4035 ; sleep 1 ; exit)  | telnet localhost 5554
-
-IP=`adb shell sudo ifconfig`
-
-echo $IP
+# AUTH=`cat /Users/runner/.emulator_console_auth_token`
+#
+# echo $AUTH
+#
+# (sleep 1 ; echo auth $AUTH ; sleep 1 ; echo redir add tcp:4035:4035 ; sleep 1 ; exit)  | telnet localhost 5554
+#
+# IP=`adb shell sudo ifconfig`
+#
+# echo $IP
 # (sleep 1 ; sudo ; sleep 1 ; ifconfig ; sleep 1 ; exit ; sleep 1 ; exit)  | adb shell
 
 # curl  -X GET \
 #       -H 'Origin: `ipconfig getifaddr en0`' \
 #       http://`ipconfig getifaddr en0`:4035/gotapi/availability
-curl  -X GET -H 'Origin: $IP' http://$IP:4035/gotapi/availability
-      
+# curl  -X GET -H 'Origin: $IP' http://$IP:4035/gotapi/availability
+
 # curl  -X GET \
 #       -H 'Origin: 10.79.2.176' \
 #       http://github.com:4035/gotapi/serviceDiscovery
-# cd dConnectManager/dConnectManager
-# chmod +x ./gradlew
-# ./gradlew dconnect-server-nano-httpd:connectedAndroidTest
-# ./gradlew dconnect-manager-app:connectedAndroidTest --stacktrace
+cd dConnectManager/dConnectManager
+chmod +x ./gradlew
+./gradlew dconnect-server-nano-httpd:connectedAndroidTest
+./gradlew dconnect-manager-app:connectedAndroidTest --stacktrace
