d4e2bde09fec0b5e5ee5f1d8b322e6c47ed24aa2
TakayukiHoshi1984 <hoshi@gclue.jp>
2020-06-30 09:24:52 +0900

    'を`に

--- emulator-install.sh
+++ emulator-install.sh
@@ -27,8 +27,8 @@ echo $AUTH
 #       -H 'Origin: `ipconfig getifaddr en0`' \
 #       http://`ipconfig getifaddr en0`:4035/gotapi/availability
 curl  -X GET \
-      -H 'Origin: 127.0.0.1' \
-      http://127.0.0.1:4035/gotapi/availability
+      -H `Origin: localhost` \
+      http://localhost:4035/gotapi/availability
       
 # curl  -X GET \
 #       -H 'Origin: 10.79.2.176' \
